632c23bdeabe665fdeeec7f83ac20288417429b5
vkuday <volkuday@hotmail.com>
2022-03-28 23:58:24 -0700

    Initial Commit

---
+++ Volymap/VolymapApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolymapApp.swift
+//  Volymap
+//
+//  Created by yupana on 3/28/22.
+//
+
+import SwiftUI
+
+@main
+struct VolymapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
