e0e4728fecf1693bc245584c5b15eea7ee8672ce
vkuday <volkuday@hotmail.com>
2022-04-05 17:28:39 -0700

    Implemented navigation link & scrollview on locationlistview

--- Volymap/LocationDetailView.swift
+++ Volymap/LocationDetailView.swift
@@ -13,62 +13,63 @@ struct LocationDetailView: View {
     let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
     
     var body: some View {
-        NavigationView {
-            VStack(spacing: 16) {
-                Image("default-banner-asset")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 120)
+        VStack(spacing: 16) {
+            Image("default-banner-asset")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 120)
+            
+            HStack {
+                Label("123 Main Street", systemImage: "mappin.and.ellipse")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
                 
-                HStack {
-                    Label("123 Main Street", systemImage: "mappin.and.ellipse")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                    
-                    Spacer()
-                }
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            Text("This is a test description.")
+                .lineLimit(3)
+                .minimumScaleFactor(0.75)
+                .frame(height: 70)
                 .padding(.horizontal)
+            
+            ZStack {
+                Capsule()
+                    .frame(height: 80)
+                    .foregroundColor(Color(.secondarySystemBackground))
                 
-                Text("This is a test description.")
-                    .lineLimit(3)
-                    .minimumScaleFactor(0.75)
-                    .padding(.horizontal)
-                
-                ZStack {
-                    Capsule()
-                        .frame(height: 80)
-                        .foregroundColor(Color(.secondarySystemBackground))
+                HStack(spacing: 20) {
+                    Button {
+                        
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "location.fill")
+                    }
                     
-                    HStack(spacing: 20) {
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "location.fill")
-                        }
+                    Link(destination: URL(string: "https://www.yupanatech.com")!) {
+                        LocationActionButton(color: .brandPrimary, imageName: "network")
+                    }
+                    
+                    Button {
                         
-                        Link(destination: URL(string: "https://www.yupanatech.com")!) {
-                            LocationActionButton(color: .brandPrimary, imageName: "network")
-                        }
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
+                    }
+                    
+                    Button {
                         
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
-                        }
-                        
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
-                        }
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
                     }
                 }
-                .padding(.horizontal)
-                
-                Text("Who's Here?")
-                    .bold()
-                    .font(.title2)
-                
+            }
+            .padding(.horizontal)
+            
+            Text("Who's Here?")
+                .bold()
+                .font(.title2)
+            
+            ScrollView {
                 LazyVGrid(columns: columns) {
                     FirstNameAvatarView(firstName: "Volkan")
                     FirstNameAvatarView(firstName: "Volkan")
@@ -77,18 +78,20 @@ struct LocationDetailView: View {
                     FirstNameAvatarView(firstName: "Volkan")
                     FirstNameAvatarView(firstName: "Volkan")
                 }
-                
-                Spacer()
             }
-            .navigationTitle("Location Name")
-            .navigationBarTitleDisplayMode(.inline)
+            
+            Spacer()
         }
+        .navigationTitle("Location Name")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct LocationDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationDetailView()
+        NavigationView {
+            LocationDetailView()
+        }
     }
 }
 
--- Volymap/LocationListView.swift
+++ Volymap/LocationListView.swift
@@ -11,32 +11,13 @@ struct LocationListView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(0..<10) { item in
-                    HStack {
-                        Image("default-square-asset")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 80, height: 80)
-                            .clipShape(Circle())
-                            .padding(.vertical, 8)
-                        VStack(alignment: .leading) {
-                            Text("Test Location Name")
-                                .font(.title2)
-                                .fontWeight(.semibold)
-                                .lineLimit(1)
-                                .minimumScaleFactor(0.75)
-                            HStack {
-                                AvatarView(size: 35)
-                                AvatarView(size: 35)
-                                AvatarView(size: 35)
-                                AvatarView(size: 35)
-                                AvatarView(size: 35)
-                            }
-                        }
-                        .padding(.leading)
+                ForEach(0..<10) { _ in
+                    NavigationLink(destination: LocationDetailView()) {
+                        LocationCell()
                     }
                 }
             }
+            .listStyle(PlainListStyle())
             .navigationTitle("Spots")
         }
     }
@@ -58,5 +39,34 @@ struct AvatarView: View {
             .scaledToFit()
             .frame(width: size, height: size)
             .clipShape(Circle())
+    }
+}
+
+struct LocationCell: View {
+    var body: some View {
+        HStack {
+            Image("default-square-asset")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+                .padding(.vertical, 8)
+            
+            VStack(alignment: .leading) {
+                Text("Test Location Name")
+                    .font(.system(size: 24))
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
+                HStack {
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                    AvatarView(size: 35)
+                }
+            }
+            .padding(.leading)
+        }
     }
 }
